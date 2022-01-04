29ef1750e10b807fb516cbec1b0c41412d1dbc7d
fr4231gm <fr4231gm@gmail.com>
2022-01-04 20:22:14 +0100

    Añadida alimentación de forma manual

--- AutomaticFeeder/AutomaticFeeder.ino
+++ AutomaticFeeder/AutomaticFeeder.ino
@@ -16,62 +16,62 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 //Configurando PAP
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
-  // Valores por defecto de las variables
+// Valores por defecto de las variables
 int numeroDeComidas = 2;  //Numero de veces que el sistema concede comida al animal
-int cantidadDeComida = 5; //Vueltas del PAP, luego intentaré ver cuantos gramos da una vuelta  
-int horaComida1 = 16;
-int minutoComida1 = 35;
-int horaComida2 = 7;
-int minutoComida2 = 30;
+int cantidadDeComida = 5; //Vueltas del PAP, luego intentaré ver cuantos gramos da una vuelta
+int horaComida1 = 17;
+int minutoComida1 = 38;
+int horaComida2 = 19;
+int minutoComida2 = 20;
 const int boton1 = 13;
 const int boton2 = 4;
 
 
 void setup() {
-  
-  
-lcd.begin(16,2);
-lcd.setCursor(17,0);
-lcd.print("Iniciando...");
-delay(2000);
-pinMode(boton1, INPUT);
-pinMode(boton2, INPUT);
-stepper.setSpeed(60);
-stepper.setAcceleration(30);
-  //Verificamos que el sistema RTC está en buenas condiciones
-   if (!rtc.begin()) {
-      lcd.clear();
-      lcd.print(("Error en el RTC"));
-      while (1);
-   }
 
-delay(1000);
-lcd.clear();
+
+  lcd.begin(16, 2);
+  lcd.setCursor(17, 0);
+  lcd.print("Iniciando...");
+  delay(2000);
+  pinMode(boton1, INPUT);
+  pinMode(boton2, INPUT);
+  stepper.setSpeed(1000);
+  stepper.setAcceleration(1000);
+  //Verificamos que el sistema RTC está en buenas condiciones
+  if (!rtc.begin()) {
+    lcd.clear();
+    lcd.print(("Error en el RTC"));
+    while (1);
+  }
+  stepper.setCurrentPosition(cantidadDeComida * 200);
+  delay(1000);
+  lcd.clear();
 
 }
 
 void printearDatos(DateTime date)
 {
-   lcd.clear();
-   lcd.setCursor(0,0);
-   lcd.print(date.hour(), DEC);
-   lcd.print(':');
-   lcd.print(date.minute(), DEC);
-   lcd.print(':');
-   lcd.print(date.second(), DEC);
-   lcd.setCursor(9,0);
-   lcd.print("Cant:");
-   lcd.setCursor(15,0);
-   lcd.print(numeroDeComidas);
-   if(numeroDeComidas == 1){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(date.hour(), DEC);
+  lcd.print(':');
+  lcd.print(date.minute(), DEC);
+  lcd.print(':');
+  lcd.print(date.second(), DEC);
+  lcd.setCursor(9, 0);
+  lcd.print("Cant:");
+  lcd.setCursor(15, 0);
+  lcd.print(numeroDeComidas);
+  if (numeroDeComidas == 1) {
     lcd.noAutoscroll();
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print("Hora: ");
     lcd.print(horaComida1);
     lcd.print(":");
     lcd.print(minutoComida1);
-   }else if(numeroDeComidas ==2){
-    lcd.setCursor(0,1);
+  } else if (numeroDeComidas == 2) {
+    lcd.setCursor(0, 1);
     lcd.print("Hora:");
     lcd.print(horaComida1);
     lcd.print(":");
@@ -80,66 +80,69 @@ void printearDatos(DateTime date)
     lcd.print(horaComida2);
     lcd.print(":");
     lcd.print(minutoComida2);
-    
-   }
+
+  }
 }
 
 void loop() {
   // Bucle principal del programa
   DateTime fecha = rtc.now();
- printearDatos(fecha);
- delay(1000);
-  
-if(numeroDeComidas == 1){
-  if(fecha.hour() == horaComida1 && fecha.minute() == minutoComida1){
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Dando de comer");
-    stepper.moveTo(cantidadDeComida*200);
-    stepper.runToPosition();
-    delay(60000);
+  printearDatos(fecha);
+  delay(1000);
+
+  if (numeroDeComidas == 1) {
+    if (fecha.hour() == horaComida1 && fecha.minute() == minutoComida1) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Dando de comer");
+      stepper.moveTo(0);
+      stepper.runToPosition();
+      stepper.setCurrentPosition(cantidadDeComida * 200);
+      delay(60000);
+    }
+
+  } else if (numeroDeComidas == 2) {
+    if (fecha.hour() == horaComida1 && fecha.minute() == minutoComida1) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Dando de comer");
+      stepper.moveTo(0);
+      stepper.runToPosition();
+      stepper.setCurrentPosition(cantidadDeComida * 200);
+      delay(60000);
+    }
+    if (fecha.hour() == horaComida2 && fecha.minute() == minutoComida2) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Dando de comer");
+      stepper.moveTo(0);
+      stepper.runToPosition();
+      stepper.setCurrentPosition(cantidadDeComida * 200);
+      delay(60000);
+    }
   }
-  
-}else if(numeroDeComidas == 2){
-  if(fecha.hour() == horaComida1 && fecha.minute() == minutoComida1){
+
+
+  if (digitalRead(boton1) == HIGH) {
     lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Dando de comer");
-    stepper.moveTo(cantidadDeComida*200);
-    stepper.runToPosition();
-    delay(60000);
-  }
-  if(fecha.hour() == horaComida2 && fecha.minute() == minutoComida2){
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Dando de comer");
-    stepper.moveTo(cantidadDeComida*200);
-    stepper.runToPosition();
-    delay(60000);
-  }
-}
-  
-  
-if(digitalRead(boton1) == HIGH){
-    lcd.clear();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("Iniciando");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print("Configuracion");
     delay(5000);
 
     //Configurar paso a paso las propiedades
     lcd.clear();
-    
-    while(digitalRead(boton1) != HIGH){
-      lcd.setCursor(0,0);
+
+    while (digitalRead(boton1) != HIGH) {
+      lcd.setCursor(0, 0);
       lcd.print("No. de comidas");
-      lcd.setCursor(0,1);
+      lcd.setCursor(0, 1);
       lcd.print(numeroDeComidas);
-      if(digitalRead(boton2) == HIGH){
-        if(numeroDeComidas == 2){
+      if (digitalRead(boton2) == HIGH) {
+        if (numeroDeComidas == 2) {
           numeroDeComidas = 1;
-        } else{
+        } else {
           numeroDeComidas = 2;
         }
         delay(250);
@@ -147,128 +150,135 @@ if(digitalRead(boton1) == HIGH){
     }
     delay(500);
     lcd.clear();
-    lcd.setCursor(0,0);
-    if(numeroDeComidas == 2){
-     
-     
-      while(digitalRead(boton1) != HIGH){
-        lcd.clear();
-         lcd.print("Horas de comidas");
-      lcd.setCursor(0,1);
-      lcd.print("Hora 1: ");
-         
-          lcd.print(horaComida1);
-          
-        if(digitalRead(boton2) == HIGH){
-         if(horaComida1 < 23){
-          horaComida1++;
-         }else{
-          horaComida1 = 00;
-         }
-         
-        }
-        delay(250);
-      }
-      delay(500);
-      
-         
-      while(digitalRead(boton1) != HIGH){
-        lcd.setCursor(0,1);
-      lcd.clear();
-         lcd.print("Horas de comidas");
-      lcd.setCursor(0,1);
-      lcd.print("Minuto 1: ");
-         lcd.print(minutoComida1);
-        if(digitalRead(boton2) == HIGH){
-         if(minutoComida1 < 59){
-          minutoComida1++;
-         }else{
-          minutoComida1 = 00;
-         }
-        }
-        delay(250);
-      }
-      delay(500);
-      while(digitalRead(boton1) != HIGH){
-        lcd.clear();
-         lcd.print("Horas de comidas");
-      lcd.setCursor(0,1);
-      lcd.print("Hora 2: ");
-         
-          lcd.print(horaComida2);
-          
-        if(digitalRead(boton2) == HIGH){
-         if(horaComida2 < 23){
-          horaComida2++;
-         }else{
-          horaComida2 = 00;
-         }
-         
-        }
-        delay(250);
-      }
-      delay(500);
-      
-         
-      while(digitalRead(boton1) != HIGH){
-        lcd.setCursor(0,1);
-      lcd.clear();
-         lcd.print("Horas de comidas");
-      lcd.setCursor(0,1);
-      lcd.print("Minuto 2: ");
-         lcd.print(minutoComida2);
-        if(digitalRead(boton2) == HIGH){
-         if(minutoComida2 < 59){
-          minutoComida2++;
-         }else{
-          minutoComida2 = 00;
-         }
-        }
-        delay(250);
-      }
-      delay(500);
-    }else {
+    lcd.setCursor(0, 0);
+    if (numeroDeComidas == 2) {
 
-    
-      while(digitalRead(boton1) != HIGH){
+
+      while (digitalRead(boton1) != HIGH) {
+        lcd.clear();
+        lcd.print("Horas de comidas");
+        lcd.setCursor(0, 1);
+        lcd.print("Hora 1: ");
+
+        lcd.print(horaComida1);
+
+        if (digitalRead(boton2) == HIGH) {
+          if (horaComida1 < 23) {
+            horaComida1++;
+          } else {
+            horaComida1 = 00;
+          }
+
+        }
+        delay(250);
+      }
+      delay(500);
+
+
+      while (digitalRead(boton1) != HIGH) {
+        lcd.setCursor(0, 1);
+        lcd.clear();
+        lcd.print("Horas de comidas");
+        lcd.setCursor(0, 1);
+        lcd.print("Minuto 1: ");
+        lcd.print(minutoComida1);
+        if (digitalRead(boton2) == HIGH) {
+          if (minutoComida1 < 59) {
+            minutoComida1++;
+          } else {
+            minutoComida1 = 00;
+          }
+        }
+        delay(250);
+      }
+      delay(500);
+      while (digitalRead(boton1) != HIGH) {
+        lcd.clear();
+        lcd.print("Horas de comidas");
+        lcd.setCursor(0, 1);
+        lcd.print("Hora 2: ");
+
+        lcd.print(horaComida2);
+
+        if (digitalRead(boton2) == HIGH) {
+          if (horaComida2 < 23) {
+            horaComida2++;
+          } else {
+            horaComida2 = 00;
+          }
+
+        }
+        delay(250);
+      }
+      delay(500);
+
+
+      while (digitalRead(boton1) != HIGH) {
+        lcd.setCursor(0, 1);
+        lcd.clear();
+        lcd.print("Horas de comidas");
+        lcd.setCursor(0, 1);
+        lcd.print("Minuto 2: ");
+        lcd.print(minutoComida2);
+        if (digitalRead(boton2) == HIGH) {
+          if (minutoComida2 < 59) {
+            minutoComida2++;
+          } else {
+            minutoComida2 = 00;
+          }
+        }
+        delay(250);
+      }
+      delay(500);
+    } else {
+
+
+      while (digitalRead(boton1) != HIGH) {
         lcd.clear();
         lcd.print("Hora de comida");
-      lcd.setCursor(0,1);
-      lcd.print("Hora: ");
-         lcd.print(horaComida1);
-        if(digitalRead(boton2) == HIGH){
-         if(horaComida1 < 23){
-          horaComida1++;
-         }else{
-          horaComida1 = 00;
-         }
+        lcd.setCursor(0, 1);
+        lcd.print("Hora: ");
+        lcd.print(horaComida1);
+        if (digitalRead(boton2) == HIGH) {
+          if (horaComida1 < 23) {
+            horaComida1++;
+          } else {
+            horaComida1 = 00;
+          }
         }
         delay(250);
       }
       delay(500);
-      
-      while(digitalRead(boton1) != HIGH){
+
+      while (digitalRead(boton1) != HIGH) {
         lcd.clear();
-         lcd.print("Hora de comida");
-      lcd.setCursor(0,1);
-      lcd.print("Minuto : ");
-         lcd.print(minutoComida1);
-        if(digitalRead(boton2) == HIGH){
-         if(minutoComida1 < 59){
-          minutoComida1++;
-         }else{
-          minutoComida1 = 00;
-         }
-       
-       }
-       delay(250);
+        lcd.print("Hora de comida");
+        lcd.setCursor(0, 1);
+        lcd.print("Minuto : ");
+        lcd.print(minutoComida1);
+        if (digitalRead(boton2) == HIGH) {
+          if (minutoComida1 < 59) {
+            minutoComida1++;
+          } else {
+            minutoComida1 = 00;
+          }
+
+        }
+        delay(250);
       }
       delay(500);
-    
-  }
-  
- }
 
- 
- 
- }
+    }
+
+  } else if (digitalRead(boton2) == HIGH) {
+    lcd.clear();
+    lcd.print("Dando de comer");
+    stepper.moveTo(0);
+    stepper.runToPosition();
+    stepper.setCurrentPosition(cantidadDeComida * 200);
+    delay(10000);
+  }
+
+
+
+}
